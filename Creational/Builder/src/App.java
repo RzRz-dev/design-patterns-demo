56461345945f7174561fac970a1fcecf396432ef
RzRz-dev <83264342+RzRz-dev@users.noreply.github.com>
2026-03-06 13:22:16 -0500

    Changed folder structure

--- Creational/Builder/src/App.java
+++ Creational/Builder/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /* The problem to solve:
+         *      Imagine you have a very modular big object, for example a house, a house can have windows,
+         *  doors, rooms, roof, floors, etc., but it can also have many things that most of the houses may or may not have.
+         *  For exmaple, garden, garage, rooftop, mini bar, basement.
+         * 
+         *  So, instead of creating a large and ugly constructor is more convenient to implement this design pattern.
+         */
         System.out.println("Hello, World!");
     }
 }
