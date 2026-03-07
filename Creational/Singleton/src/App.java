114cb980326864c6711ced664735445eeeb2acd9
RzRz-dev <83264342+RzRz-dev@users.noreply.github.com>
2026-03-06 19:03:27 -0500

    Added Singleton Design Pattern

---
+++ Creational/Singleton/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        * Singleton is a design pattern that lets you ensure that a class has only one instance, providing global access point to this
+        * instance.
+        * 
+        * In this example a simple logger system is going to be implemented.
+        */
+    }
+}
