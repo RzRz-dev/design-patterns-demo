a3ad9032df7eba9628f080af6680f6ecfcfd1c87
RzRz-dev <83264342+RzRz-dev@users.noreply.github.com>
2026-03-06 19:16:41 -0500

    Added singleoton pattern with an example of Logging through a single logging system

--- Creational/Singleton/src/App.java
+++ Creational/Singleton/src/App.java
@@ -1,3 +1,5 @@
+import model.Logger;
+
 public class App {
     public static void main(String[] args) throws Exception {
         /*
@@ -6,5 +8,12 @@ public class App {
         * 
         * In this example a simple logger system is going to be implemented.
         */
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application started");
+        logger2.log("User logged in");
+
+        System.out.println("Same instance? " + (logger1 == logger2));
     }
 }
--- Creational/Singleton/src/model/Logger.java
+++ Creational/Singleton/src/model/Logger.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Logger {
+    public static Logger instance;
     
+    private Logger(){
+        // This is private so no constructor can be called, this prevents Logger variable = new Logger();
+    }
+
+    public static Logger getInstance(){
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message){
+        System.out.println("[LOG]: "+message);
+    }
 }
